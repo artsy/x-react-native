b8e33e9b62d2ec343b8569397f74fbeb6905ac81
Maxim Cramer <mennenia@gmail.com>
2018-07-07 14:57:45 +0100

    images are now working

--- workshops/primitives/BreadButter_2_components/src/Components/BreadItem.tsx
+++ workshops/primitives/BreadButter_2_components/src/Components/BreadItem.tsx
@@ -4,18 +4,16 @@ import { StyleSheet, Image, Text, View } from "react-native"
 interface BreadProps {
   bread: {
     name: string
-    price: number
+    price: string
     bakery: string
     img: string
   }
 }
 export default class BreadItem extends React.Component<BreadProps, any> {
   render() {
-    const thingy =
-      "https://photos-4.dropbox.com/t/2/AAC_g4sDQLcxcO38tUdcf0nD_ECeF3zq-gN33J2Zg2m_4g/12/6135859/jpeg/32x32/1/_/1/2/ciabatta.jpg/EO-IygQY6uIKIAcoBw/ZfsZ0IgoG59Cj3hV2KSZjIbso2VbMtdREnhHh5pRWQM?size=2048x1536&size_mode=3"
     return (
       <View style={styles.container}>
-        <Image source={{ uri: thingy }} resizeMode="center" style={styles.imageStyle} />
+        <Image source={{ uri: this.props.bread.img }} resizeMode="center" style={styles.imageStyle} />
         <View style={styles.metadataContainer}>
           <View>
             <Text style={styles.breadTitle}>{this.props.bread.name}</Text>
