4050a20b30c6a5b3e0a617b4ddabbc35f5f08728
Maxim Cramer <mennenia@gmail.com>
2018-07-08 10:41:26 +0100

    polish first part

--- workshops/primitives/BreadButter_1_start/src/App.tsx
+++ workshops/primitives/BreadButter_1_start/src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react"
 import { Component } from "react"
 import { FlatList, StyleSheet, Text, View } from "react-native"
-import data from "./data/breads"
+
+import breads from "./data/breads"
 
 type Props = {}
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <FlatList data={data} renderItem={({ item }) => <Text>{item.name}</Text>} />
+        <FlatList data={breads} renderItem={({ item }) => <Text>{item.name}</Text>} />
       </View>
     )
   }
@@ -16,8 +17,6 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    marginTop: 40,
-    marginLeft: 20,
+    paddingTop: 30,
   },
 })
